6bf98e7014f020cfa715699d1911dbc754c2765b
Jalepino1 <hengeno818@gmail.com>
2024-02-21 11:46:15 -0600

    GUI changes

--- GUI.py
+++ GUI.py
@@ -9,17 +9,17 @@ root.geometry("800x500")
 root.title("Rail Fence Decryption and Encryption")
 
 
-textbox = tk.Text(root, height =6)
+textbox = tk.Text(root, height=2, font=('Comic-sans', 25))
 textbox.pack(padx=20,pady=20)
 
 
 buttonframe = tk.Frame(root)
 buttonframe.columnconfigure(0,weight=1)
 buttonframe.columnconfigure(1,weight=1) 
-decryption = tk.Button(buttonframe, text="Decrypt Text",font=('Comic-sans', 16))
-decryption.grid(row=0,column=0, sticky=tk.W+tk.E)
-encryption = tk.Button(buttonframe, text="Encrypt Text",font=('Comic-sans', 16))
-encryption.grid(row=0,column=1,sticky=tk.W+tk.E)
+decryption = tk.Button(buttonframe, text="Decrypt Text",font=('Comic-sans', 30))
+decryption.grid(row=1,column=0, sticky=tk.W+tk.E)
+encryption = tk.Button(buttonframe, text="Encrypt Text",font=('Comic-sans', 30))
+encryption.grid(row=1,column=1,sticky=tk.W+tk.E)
 buttonframe.pack(fill='x')
 
 
